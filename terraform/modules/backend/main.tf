10881986fde8daeef19e00331c73aa02aa8fc6e2
Dayo <iamdayoaderinto@gmail.com>
2025-05-21 10:59:37 +0100

    Separated my envs config

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -1,19 +1,15 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
+}
 
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
-
-  tags = var.tags
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
